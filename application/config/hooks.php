c8c8b524822d0d7237b7b29695d3d6de85a35c41
Clark Steven <edongclarksteven@gmail.com>
2026-09-04 21:03:53 +0800

    cc

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -2,45 +2,17 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
-| -------------------------------------------------------------------------
-| Hooks
-| -------------------------------------------------------------------------
-| This file lets you define "hooks" to extend CI without hacking the core
-| files.  Please see the user guide for info:
+|| -------------------------------------------------------------------------
+|| Hooks
+|| -------------------------------------------------------------------------
+|| This file lets you define "hooks" to extend CI without hacking the core
+|| files.  Please see the user guide for info:
+||
+||	https://codeigniter.com/user_guide/general/hooks.html
 |
-|	https://codeigniter.com/user_guide/general/hooks.html
-|
+| Pageant-era hooks (SchemaSync, BrowserRequestGuard, AcademicOfficerGuard,
+| MaintenanceMode) removed when this project was split off as the nexam
+| TOS-aligned exam builder. Re-enable here if hooks are needed again.
 */
 
-$config['enable_hooks'] = TRUE;
-
-// Auto-apply idempotent schema deltas (adds missing columns) so production
-// never needs a manual ALTER. See application/hooks/SchemaSync.php.
-$hook['post_controller_constructor'][] = array(
-    'class'    => 'SchemaSync',
-    'function' => 'migrate',
-    'filename' => 'SchemaSync.php',
-    'filepath' => 'hooks',
-);
-
-// Temporarily disabled maintenance mode hook
-// $hook['pre_controller'] = array(
-//     'class'    => 'MaintenanceMode',
-//     'function' => 'check_maintenance',
-//     'filename' => 'MaintenanceMode.php',
-//     'filepath' => 'hooks',
-// );
-
-$hook['post_controller_constructor'][] = array(
-    'class'    => 'BrowserRequestGuard',
-    'function' => 'protect',
-    'filename' => 'BrowserRequestGuard.php',
-    'filepath' => 'hooks',
-);
-
-$hook['post_controller_constructor'][] = array(
-    'class'    => 'AcademicOfficerGuard',
-    'function' => 'restrict',
-    'filename' => 'AcademicOfficerGuard.php',
-    'filepath' => 'hooks',
-);
+$config['enable_hooks'] = FALSE;
--- application/config/routes.php
+++ application/config/routes.php
@@ -1,19 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-
-$route['default_controller'] = 'Login';
+$route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-$route['leaderboard'] = 'login/leaderboard';
-$route['leaderboard/data'] = 'login/leaderboard_data';
-
-$route['dashboard'] = 'judgedashboard';
-$route['judgedashboard'] = 'judgedashboard';
-$route['judgedashboard/score/(:num)'] = 'judgedashboard/score/$1';
-$route['judgedashboard/submit_score'] = 'judgedashboard/submit_score';
-$route['judgedashboard/tabulation'] = 'judgedashboard/tabulation';
-$route['judgedashboard/judge_results'] = 'judgedashboard/judge_results';
-$route['judgedashboard/my_results'] = 'judgedashboard/my_results';
-$route['judgedashboard/logout'] = 'judgedashboard/logout';

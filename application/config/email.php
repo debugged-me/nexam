c4600964968ec4894ccb8c8a98f1f2580ea511fa
Clark Steven <edongclarksteven@gmail.com>
2026-09-11 19:10:15 +0800

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -1,9 +1,9 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
 $config['protocol']     = 'smtp';
-$config['smtp_host']    = 'mail.srmsportal.com';
-$config['smtp_user']    = 'tagumdoc@srmsportal.com';
-$config['smtp_pass']    = getenv('SRMS_SMTP_PASS') ?: 'moth34board';
+$config['smtp_host']    = 'mail.mati.gov.ph';
+$config['smtp_user']    = 'nexam@mati.gov.ph';
+$config['smtp_pass']    = getenv('NEXAM_SMTP_PASS') ?: 'nexamcapstone';
 $config['smtp_port']    = 465;
 $config['smtp_crypto']  = 'ssl';
 $config['smtp_timeout'] = 10;
@@ -12,5 +12,5 @@ $config['charset']      = 'utf-8';
 $config['newline']      = "\r\n";
 $config['crlf']         = "\r\n";
 $config['wordwrap']     = true;
-$config['from_email']   = 'tagumdoc@srmsportal.com';
+$config['from_email']   = 'nexam@mati.gov.ph';
 $config['from_name']    = 'nexam';
